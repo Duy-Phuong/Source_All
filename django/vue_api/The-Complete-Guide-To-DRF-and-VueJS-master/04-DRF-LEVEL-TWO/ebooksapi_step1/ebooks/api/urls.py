4224f7b2003269c2bdc437713d4f31c8a7928f4e
phuong <tranduyphuong20100@gmail.com>
2019-07-29 01:06:02 +0700

    3. The GenericAPIView Class and Mixins

--- django/vue_api/The-Complete-Guide-To-DRF-and-VueJS-master/04-DRF-LEVEL-TWO/ebooksapi_step1/ebooks/api/urls.py
+++ django/vue_api/The-Complete-Guide-To-DRF-and-VueJS-master/04-DRF-LEVEL-TWO/ebooksapi_step1/ebooks/api/urls.py
@@ -7,15 +7,15 @@ urlpatterns = [
          EbookListCreateAPIView.as_view(), 
          name="ebook-list"),
 
-    path("ebooks/<int:pk>/", 
-         EbookDetailAPIView.as_view(), 
-         name="ebook-detail"),
-
-    path("ebooks/<int:ebook_pk>/review/", 
-         ReviewCreateAPIView.as_view(), 
-         name="ebook-review"),
-
-    path("reviews/<int:pk>/", 
-         ReviewDetailAPIView.as_view(), 
-         name="review-detail")
+    # path("ebooks/<int:pk>/",
+    #      EbookDetailAPIView.as_view(),
+    #      name="ebook-detail"),
+    #
+    # path("ebooks/<int:ebook_pk>/review/",
+    #      ReviewCreateAPIView.as_view(),
+    #      name="ebook-review"),
+    #
+    # path("reviews/<int:pk>/",
+    #      ReviewDetailAPIView.as_view(),
+    #      name="review-detail")
 ]
