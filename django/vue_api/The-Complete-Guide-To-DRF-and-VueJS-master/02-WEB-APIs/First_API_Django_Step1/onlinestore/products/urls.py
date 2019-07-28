23a9b1632a3cfbfa7de92b0c1a1f1b1bd43ab108
phuong <tranduyphuong20100@gmail.com>
2019-07-28 17:08:36 +0700

    first commit

---
+++ django/vue_api/The-Complete-Guide-To-DRF-and-VueJS-master/02-WEB-APIs/First_API_Django_Step1/onlinestore/products/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+# from .views import (manufacturer_detail, manufacturer_list,
+#                     product_detail, product_list)
+
+# urlpatterns = [
+#     path("products/", product_list, name="product-list"),
+#     path("products/<int:pk>/", product_detail, name="product-detail"),
+#
+#     path("manufacturers/", manufacturer_list, name="manufacturer-list"),
+#     path("manufacturers/<int:pk>/", manufacturer_detail, name="manufacturer-detail")
+# ]
+
+# step 1
+from .views import ProductDetailView, ProductListView
+
+urlpatterns = [
+    path("products/", ProductListView.as_view(), name="product-list"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+
+]
