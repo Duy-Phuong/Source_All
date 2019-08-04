f1b769ca426d09359d82c6171c7314b177abcecd
phuong <tranduyphuong20100@gmail.com>
2019-08-05 01:05:42 +0700

    step 2

--- react/robot/robofriends/src/Hello.js
+++ react/robot/robofriends/src/Hello.js
@@ -2,16 +2,26 @@ import React, { Component } from "react";
 import "./App.css";
 import "tachyons";
 
-class Hello extends Component {
-  render() {
-    return (
-      <div className="tc i f1">
-        <p>
-          Hello msg <code>{this.props.gretting}</code>
-        </p>
-      </div>
-    );
-  }
-}
+// class Hello extends Component {
+//   render() {
+//     return (
+//       <div className="tc i f1">
+//         <p>
+//           Hello msg <code>{this.props.gretting}</code>
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+const Hello = props => {
+  return (
+    <div className="tc i f1">
+      <p>
+        Hello msg <code>{props.gretting}</code>
+      </p>
+    </div>
+  );
+};
 
 export default Hello;
