4f7a2a3d9d549a91884f1fd94a4e16b0826cdd55
phuong <tranduyphuong20100@gmail.com>
2019-08-05 22:43:04 +0700

    modify

--- react/robot/robofriends/src/Card.js
+++ react/robot/robofriends/src/Card.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./Card.css";
 import "tachyons";
 
-const Card = props => {
-  // const {name, email} = props;
+const Card = ({ name, email, id }) => {
+  //   const { name, email, id } = props;
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robot" src={`https://robohash.org/${props.id}?200x200`} />
+      <img alt="robot" src={`https://robohash.org/${id}?200x200`} />
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.email}</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
