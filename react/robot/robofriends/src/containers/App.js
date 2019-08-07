4e8f72945986cff96ea19e83722654cb65d0a6c5
phuong <tranduyphuong20100@gmail.com>
2019-08-07 22:44:56 +0700

    21. Redux Store and Provider

--- react/robot/robofriends/src/containers/App.js
+++ react/robot/robofriends/src/containers/App.js
@@ -16,6 +16,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    console.log(this.props.store);
     console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => {
--- react/robot/robofriends/src/index.js
+++ react/robot/robofriends/src/index.js
@@ -8,6 +8,9 @@ import App from "./containers/App";
 
 import * as serviceWorker from "./serviceWorker";
 import { robots } from "./robots";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { requestRobots, searchRobots } from "./reducers";
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -34,8 +37,14 @@ import { robots } from "./robots";
 // step 4 end
 
 // step 5 start
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
 // step 5 end
+
+// step 6 start
+const store = createStore(searchRobots);
+
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
+// step 6 end
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
